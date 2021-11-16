58cf55cf8cd7f1481ea792dc64d12e649544bc57
ivanfreitasdossantos <ivan@tenil.com.br>
2021-11-16 13:40:01 -0300

    Update insert_data.sql

--- src/main/resources/insert_data.sql
+++ src/main/resources/insert_data.sql
@@ -1,6 +1,1 @@
-INSERT INTO CLIENT ( name, cpf, birth)
-VALUES ('value1', 'value2', '2016-02-06 ');
-INSERT INTO CLIENT ( name, cpf, birth)
-VALUES ('value1', 'value2', '2016-02-06 ');
-INSERT INTO CLIENT ( name, cpf, birth)
-VALUES ('value1', 'value2', '2016-02-06 ');
+
